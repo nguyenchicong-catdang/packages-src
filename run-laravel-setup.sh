f106c6c7bf92bf73e7e20e5b1f8a6c57ca600071
nguyenchicong <nguyenchicong.catdang@gmail.com>
2026-01-30 19:41:02 +0700

    update packages

---
+++ run-laravel-setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# $HOME/git/packages-app/package-src/setup/setup.sh
+bash "$HOME/git/packages-app/packages-src/setup/setup.sh"
--- wp-plugin/laravel-sidebar-html/laravel-sidebar-html.php
+++ wp-plugin/laravel-sidebar-html/laravel-sidebar-html.php
@@ -1,1 +1,25 @@
 <?php
+/*
+ * Plugin Name: LARAVEL SIDEBAR HTML
+ */
+
+add_action('admin_menu', function() {
+    add_menu_page(
+        'Laravel Sidebar Html',
+        'Laravel Sidebar Html',
+        'manage_options',
+        'laravel-sidebar-html', // Slug chính
+        'render_render_laravel_sidebar_html',
+        'dashicons-html',
+        6
+    );
+});
+
+function render_render_laravel_sidebar_html() {
+    echo '<div class="wrap"><h1>Laravel Sidebar Content</h1>';
+    
+    // Nếu bạn muốn render Blade component từ Provider đã đăng ký:
+    // echo view('wpcomp::index')->render(); 
+    
+    echo '</div>';
+}
